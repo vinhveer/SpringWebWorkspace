90cc6b699e2eb7b17ec3b610cb8d49e508b80465
Vinh Veer <vinhveer123@gmail.com>
2024-06-21 03:34:12 +0700

    Upload

---
+++ webNetflix/src/test/java/com/example/webnetflix/WebNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webnetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNetflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
